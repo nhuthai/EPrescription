cbcd296cfb9f743905702116ed97e22589ccf956
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2018-04-30 10:47:56 +0200

    Define Disease class

---
+++ Medicine/Controller/Disease.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicine
+{
+    public class Disease
+    {
+        public String name = "";
+        public String notice = "";
+
+        public Disease(String name)
+        {
+            this.name = name;
+        }
+
+        public Disease(String name, String notice)
+        {
+            this.name = name;
+            this.notice = notice;
+        }
+    }
+}
