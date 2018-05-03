f85bb3fa37cb16e70912ea705aac485d11e5f819
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2018-05-03 09:20:16 +0200

    Add Connect DataBase class

---
+++ Medicine/Model/ConnectDB.cs
@@ -0,0 +1,33 @@
+﻿using Medicine.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicine
+{
+    public class ConnectDB
+    {
+        private String connString = ConfigurationManager.ConnectionStrings["connectString"].ConnectionString;
+
+        public ConnectDB()
+        {
+
+        }
+
+        public void startSess(Query query)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand(query.ToString(), conn);
+                command.Connection.Open();
+
+                query.reader = command.ExecuteReader();
+                //command.ExecuteNonQuery();
+            }
+        }
+    }
+}
