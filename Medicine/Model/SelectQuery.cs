79eb97d644a1714f5f9da39618273f74b8e76b19
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2018-05-04 10:14:49 +0200

    Add Query subclass for Select query

---
+++ Medicine/Model/SelectQuery.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicine.Model
+{
+    public class SelectQuery : Query
+    {
+        public SelectQuery(String type, String table, String attribute, ArrayList Params)
+        {
+            this.type = type;
+            this.table = table;
+            this.attribute = attribute;
+            this.Params = Params;
+        }
+
+        public SelectQuery(String query, ArrayList Params)
+        {
+            this.queryString = query;
+            this.Params = Params;
+        }
+
+        public override bool run(bool manualOpenClose)
+        {
+            if (!manualOpenClose)
+                this.conn = openConnection(this.conn);
+            // Select simple doctor
+            try
+            {
+                SqlCommand cmd = new SqlCommand(queryString, conn);
+                foreach(Tuple<String, String> t in Params)
+                {
+                    cmd.Parameters.AddWithValue(t.Item1, t.Item2);
+                }
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                conn.Close();
+                return false;
+            }
+            return true;
+        }
+    }
+}
